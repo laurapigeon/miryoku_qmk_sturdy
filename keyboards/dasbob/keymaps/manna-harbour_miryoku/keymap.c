cd3dbbb96f5845ebfcbbc7f5e7e4590503722b8b
laurapigeon <laurahannah44@gmail.com>
2023-12-26 23:27:26 +0000

    added unicode, trouble switching to uk keycodes

--- keyboards/dasbob/keymaps/manna-harbour_miryoku/keymap.c
+++ keyboards/dasbob/keymaps/manna-harbour_miryoku/keymap.c
@@ -3,10 +3,3 @@
 
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
-
-// This globally defines all key overrides to be used
-const key_override_t **key_overrides = (const key_override_t *[]){
-    &delete_key_override,
-    NULL // Null terminate the array of overrides!
-};
--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -4,20 +4,23 @@
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
+#include "keymap_uk.h"
+
+#define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX
 
 #undef MIRYOKU_LAYER_BASE
 #define MIRYOKU_LAYER_BASE \
-KC_V,              KC_M,              KC_L,              KC_C,              KC_P,              KC_B,              KC_SLSH,           KC_U,              KC_O,              KC_Q,              \
-LGUI_T(KC_S),      LALT_T(KC_T),      LCTL_T(KC_R),      LSFT_T(KC_D),      KC_Y,              KC_F,              LSFT_T(KC_N),      LCTL_T(KC_E),      LALT_T(KC_A),      LGUI_T(KC_I),      \
-LT(U_BUTTON,KC_X), ALGR_T(KC_K),      KC_J,              KC_G,              KC_W,              KC_Z,              KC_H,              KC_COMM,           ALGR_T(KC_DOT),    LT(U_BUTTON,KC_QUOT),\
-U_NP,              U_NP,              LT(U_MEDIA,KC_ESC),LT(U_NAV,KC_SLSH), LT(U_MOUSE,KC_TAB),LT(U_SYM,KC_BSPC), LT(U_NUM,KC_SPC),  LT(U_FUN,KC_ENT),  U_NP,              U_NP
+UK_V,              UK_M,              UK_L,              UK_C,              UK_P,              UK_B,              UK_SLSH,           UK_U,              UK_O,              UK_Q,              \
+LGUI_T(UK_S),      LALT_T(UK_T),      LCTL_T(UK_R),      LSFT_T(UK_D),      UK_Y,              UK_F,              LSFT_T(UK_N),      LCTL_T(UK_E),      LALT_T(UK_A),      LGUI_T(UK_I),      \
+LT(U_BUTTON,UK_X), ALGR_T(UK_K),      UK_J,              UK_G,              UK_W,              UK_Z,              UK_H,              UK_COMM,           ALGR_T(UK_DOT),    LT(U_BUTTON,UK_QUOT),\
+U_NP,              U_NP,              LT(U_MEDIA,KC_ESC),LT(U_NAV,UK_SLSH), LT(U_MOUSE,KC_TAB),LT(U_SYM,KC_BSPC), LT(U_NUM,KC_SPC),  LT(U_FUN,KC_ENT),  U_NP,              U_NP
 
 #undef MIRYOKU_LAYER_NUM
 #define MIRYOKU_LAYER_NUM \
-KC_LBRC,           KC_4,              KC_5,              KC_6,              KC_RBRC,           U_NA,              TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),    TD(U_TD_BOOT),     \
-KC_SCLN,           KC_1,              KC_2,              KC_3,              KC_EQL,            U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
-KC_GRV,            KC_7,              KC_8,              KC_9,              KC_BSLS,           U_NA,              TD(U_TD_U_NUM),    TD(U_TD_U_NAV),    KC_ALGR,           U_NA,              \
-U_NP,              U_NP,              KC_DOT,            KC_0,              KC_MINS,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+UK_LBRC,           UK_4,              UK_5,              UK_6,              UK_RBRC,           U_NA,              TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),    TD(U_TD_BOOT),     \
+UK_SCLN,           UK_1,              UK_2,              UK_3,              UK_EQL,            U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
+UK_GRV,            UK_7,              UK_8,              UK_9,              UK_BSLS,           U_NA,              TD(U_TD_U_NUM),    TD(U_TD_U_NAV),    KC_ALGR,           U_NA,              \
+U_NP,              U_NP,              UK_DOT,            UK_0,              UK_MINS,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #undef MIRYOKU_LAYER_NAV
 #define MIRYOKU_LAYER_NAV \
@@ -28,10 +31,10 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 
 #undef MIRYOKU_LAYER_SYM
 #define MIRYOKU_LAYER_SYM \
-KC_LCBR,           KC_AMPR,           KC_ASTR,           KC_LPRN,           KC_RCBR,           U_NA,              TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),    TD(U_TD_BOOT),     \
-KC_COLN,           KC_DLR,            KC_PERC,           KC_CIRC,           KC_PLUS,           U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
-KC_TILD,           KC_EXLM,           KC_AT,             KC_HASH,           KC_PIPE,           U_NA,              TD(U_TD_U_SYM),    TD(U_TD_U_MOUSE),  KC_ALGR,           U_NA,              \
-U_NP,              U_NP,              KC_LPRN,           KC_RPRN,           KC_UNDS,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+UK_LCBR,           UK_AMPR,           UK_ASTR,           UC(0xD7),          UK_RCBR,           U_NA,              TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),    TD(U_TD_BOOT),     \
+UK_COLN,           UK_DLR,            UK_PERC,           UK_CIRC,           UK_PLUS,           U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
+UK_TILD,           UK_EXLM,           UK_AT,             UK_HASH,           UK_PIPE,           U_NA,              TD(U_TD_U_SYM),    TD(U_TD_U_MOUSE),  KC_ALGR,           U_NA,              \
+U_NP,              U_NP,              UK_LPRN,           UK_RPRN,           UK_UNDS,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #undef MIRYOKU_LAYER_MOUSE
 #define MIRYOKU_LAYER_MOUSE \
@@ -45,7 +48,7 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 KC_F11,            KC_F4,             KC_F5,             KC_F6,             KC_PSCR,           U_NA,              TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),    TD(U_TD_BOOT),     \
 KC_F10,            KC_F1,             KC_F2,             KC_F3,             KC_SCRL,           U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
 KC_F12,            KC_F7,             KC_F8,             KC_F9,             KC_PAUS,           U_NA,              TD(U_TD_U_FUN),    TD(U_TD_U_MEDIA),  KC_ALGR,           U_NA,              \
-U_NP,              U_NP,              KC_APP,            KC_SLSH,           KC_TAB,            U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+U_NP,              U_NP,              KC_APP,            UK_SLSH,           KC_TAB,            U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #undef MIRYOKU_LAYER_MEDIA
 #define MIRYOKU_LAYER_MEDIA \
@@ -56,7 +59,7 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 
 #undef MIRYOKU_LAYER_TAP
 #define MIRYOKU_LAYER_TAP \
-KC_A,              KC_Z,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,              \
-KC_Q,              KC_S,              KC_D,              KC_F,              KC_G,              KC_H,              KC_J,              KC_K,              KC_L,              KC_M,              \
-KC_W,              KC_X,              KC_C,              KC_V,              KC_B,              KC_N,              KC_COMM,           KC_DOT,            KC_SLSH,           KC_QUOT,           \
-U_NP,              U_NP,              TD(U_TD_U_BASE),   KC_SLSH,           KC_LSFT,           KC_BSPC,           KC_SPC,            KC_ENT,            U_NP,              U_NP
+UK_A,              UK_Z,              UK_E,              UK_R,              UK_T,              UK_Y,              UK_U,              UK_I,              UK_O,              UK_P,              \
+UK_Q,              UK_S,              UK_D,              UK_F,              UK_G,              UK_H,              UK_J,              UK_K,              UK_L,              UK_M,              \
+UK_W,              UK_X,              UK_C,              UK_V,              UK_B,              UK_N,              UK_COMM,           UK_DOT,            UK_SLSH,           UK_QUOT,           \
+U_NP,              U_NP,              TD(U_TD_U_BASE),   UK_SLSH,           KC_LSFT,           KC_BSPC,           KC_SPC,            KC_ENT,            U_NP,              U_NP
